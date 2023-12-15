eb3babc12c9613272c26797d3b4c0796d3260c26
hht <2459027626@qq.com>
2023-12-15 11:57:47 +0800

    fix

--- src/views/echartsEdit/demoAce/index.vue
+++ src/views/echartsEdit/demoAce/index.vue
@@ -39,6 +39,7 @@ export default {
                 initLine02,
             },
             annotaions: [],
+            errorList: [],
         };
     },
 
@@ -49,7 +50,7 @@ export default {
         //初始化ace编辑器
         this.editor = ace.edit(this.$refs.ace, {
             maxLines: 50, // 最大行数，超过会自动出现滚动条
-            minLines: 10, // 最小行数，还未到最大行数时，编辑器会自动伸缩大小
+            minLines: 12, // 最小行数，还未到最大行数时，编辑器会自动伸缩大小
             fontSize: 14, // 编辑器内字体大小
             theme: 'ace/theme/kuroir', // 默认设置的主题
             mode: 'ace/mode/json5', // 默认设置的语言模式
@@ -62,16 +63,22 @@ export default {
         })
         this.editor.on('change', (val) => {
             // let str = this.editor.setValue(this.item, -1)
-            this.item=this.editor.getValue()
+            this.item = this.editor.getValue()
             if (this.item) {
-
                 this.validateJavasriptSyntax(this.item)
+                // this.checkEsprima(this.item)
             }
+
+
         })
+        // this.editor.on('load', (val) => {
+        //    console.log(val,111111999);
+        // })
         this.editor.setOption("mode", "ace/mode/javascript");
         this.id = this.$route.query.id
         this.item = this.getText(this.id)
         this.editor.setValue(this.item, -1)
+
         this.renderEchartsDemo()
         // this.editor.getSession().setAnnotations([{
         //     row: 1,
@@ -127,9 +134,10 @@ export default {
                 acorn.parse(str, {
                     sourceType: 'module',
                     // locations: true,
-                    // onError: (error) => {
-                    //     throw new SyntaxError(error.message);
-                    // },
+                    onError: (error) => {
+                        this.errorList.push(error)
+                        // throw new SyntaxError(error);
+                    },
                 });
                 // console.log(acorn.parse(code, { ecmaVersion: 2020 }), 99999);
             } catch (error) {
@@ -137,7 +145,7 @@ export default {
                 console.log("validateJavasriptSyntax", error, 11111222333);
                 let annotationList = [];
                 let annotation = {
-                    row: error?.loc?.line - 1,
+                    row: error?.loc?.line - 2,
                     column: error?.loc?.column,
                     type: "error",
                     text: error?.message,
@@ -148,6 +156,22 @@ export default {
                 this.editor.getSession().setAnnotations(annotationList);
             }
         },
+        // checkEsprima(code) {
+        //     const esprima = require('esprima');
+        //     let str = String(code);
+        //     console.log(checkSyntax(str), 111);
+        //     function checkSyntax(code) {
+        //         try {
+        //             esprima.parseScript(code, { tolerant: true });
+        //             return [];
+        //         } catch (error) {
+        //             return error.errors || [error];
+        //         }
+        //     }
+
+
+
+        // },
         getText(id) {
             let javascript = this.echartsObject[id];
             javascript = `${javascript}`.replace(/echarts__WEBPACK_IMPORTED_MODULE_0__/g, 'echarts')
@@ -183,9 +207,9 @@ export default {
         margin-right: 30px;
     }
 
-    /deep/ .ace_editor .ace-kuroir {
-        height: 70vh !important;
-    }
+    // /deep/ .ace_editor .ace-kuroir {
+    //     height: 70vh !important;
+    // }
 
 }
 </style>
